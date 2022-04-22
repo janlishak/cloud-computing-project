f2be669cd1c605c8c3618f11cdf3f416630e48a8
Jan Lishak <35602700+janlishak@users.noreply.github.com>
2022-04-22 18:27:50 +0100

    print webserver node hostname

--- nodejs/src/test.js
+++ nodejs/src/test.js
@@ -1,11 +1,16 @@
-//first application
+// first application
 
 const express = require('express')
 const app = express()
 const port = 3000
 
+// get the hostname of the application 
+var os = require('os');
+var hostname = os.hostname();
+
+
 app.get('/', (req, res) => {
-  res.send('Hello Jan!')
+  res.send('Hello Jan! From ' + hostname)
 })
 
 app.listen(port, () => {
